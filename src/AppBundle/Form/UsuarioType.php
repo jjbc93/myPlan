90e6b10b209ed62c86a5a3c57dc6e982f8107236
jjbc93 <juanjobernabe93@gmail.com>
2016-06-01 00:11:44 +0200

    Cambios Vista y formulario usuario
    Cambios en layout por defecto y crear formulario a partir de una entidad

---
+++ src/AppBundle/Form/UsuarioType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nick')
+            ->add('nombre')
+            ->add('apellidos')
+            ->add('correo')
+            ->add('clave')
+            /*->add('rol')
+            ->add('imagen')*/
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Usuario'
+        ));
+    }
+}
