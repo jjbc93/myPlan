6c6240489fc801ecc510aaf8b31eec8e1c9ddda3
jjbc93 <juanjobernabe93@gmail.com>
2016-06-04 13:01:36 +0200

    Registro de Usuarios estable
    Tras varios dias con problemas he conseguido volver estable la
    aplicación modificando archivos del routing y el security que estaban
    mal configurados, ahora ya puedes registrar nuevos usuarios en la
    aplicación

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,22 +5,55 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Usuario;
+use AppBundle\Form\UsuarioType;
 
 class UserController extends Controller
 {   
     /**
+     * Controlador encargado de la acción de login de los usuarios,
+     * con validación de symfony3 y formulario de registro
      * @Route("/login", name="login")
      */
     
     public function loginAction(Request $request)
     {
+        //Autentificación de Symfony3
         $authenticationUtils = $this->get("security.authentication_utils");
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUserName =$authenticationUtils->getLastUsername();
+        //Formulario para registrar un nuevo usuario en la aplicación
+        $user = new Usuario();
+        $form = $this->createForm(UsuarioType::class,$user);
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+           /* $user = new Usuario();
+            $user->setNick($form->get("nick")->getData());
+            $user->setNombre($form->get("nombre")->getData());
+            $user->setApellidos($form->get("apellidos")->getData());
+            $user->setCorreo($form->get("correo")->getData());
+            $user->setClave($form->get("clave")->getData());*/
+            $user->setRol("ROLE_USER");
+            /*$user->setImagen(null);*/
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $flush = $em->flush();
+            if($flush==null){
+                 $status = "Gracias ".$user->getNombre()." desde ahora formas parte de myplan, este atento a su correo para nuevos eventos actulizaciones";
+            }else{
+                $status = "Lo sentimos no se ha podido dar de alta revise los campos y vuelva a intentarlo";
+            }
+           
+        }else{
+            $status = "formulario no valido";
+        }
+        
         
         return $this->render('default/user.html.twig',array(
             "error" => $error,
-            "lastUserName" => $lastUserName
+            "lastUserName" => $lastUserName,
+            "form" => $form->createView(),
             
         ));
     }
--- src/AppBundle/Form/UsuarioType.php
+++ src/AppBundle/Form/UsuarioType.php
@@ -5,6 +5,13 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
 
 class UsuarioType extends AbstractType
 {
@@ -15,11 +22,23 @@ class UsuarioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nick')
-            ->add('nombre')
-            ->add('apellidos')
-            ->add('correo')
-            ->add('clave')
+            ->add('nick',TextType::class, array("label"=>"Nick","required"=>"required","attr" => array(
+            "class" =>"form-nick form-control",
+            )))
+            ->add('nombre',TextType::class, array("label"=>"Nombre","required"=>"required","attr" => array(
+            "class" =>"form-nombre form-control",
+            )))
+            ->add('apellidos',TextType::class, array("label"=>"Apellidos","required"=>"required","attr" => array(
+            "class" =>"form-apellidos form-control",
+            )))
+            ->add('correo',EmailType::class, array("label"=>"Email","required"=>"required","attr" => array(
+            "class" =>"form-correo form-control",
+            )))
+            ->add('clave',PasswordType::class, array("label"=>"Contraseña","required"=>"required","attr" => array(
+            "class" =>"form-password form-control",
+            )))
+            ->add('enviar',SubmitType::class, array("attr" =>array(
+            "class" =>"form-submit btn btn-success")))
             /*->add('rol')
             ->add('imagen')*/
         ;
